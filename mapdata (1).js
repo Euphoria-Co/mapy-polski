58c73fe0f8846383cac66bff44f093a91393d6dc
ViciousCircle-Github <Stinnnkey0@gmail.com>
2020-03-22 17:02:04 +0100

    first commmit

---
+++ mapdata (1).js
@@ -0,0 +1,125 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "300", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ff0000",
+    state_description: "",
+    state_color: "white",
+    state_hover_color: "#ff0000",
+    state_url: "",
+    border_size: "2",
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    location_description: "",
+    location_url: "",
+    location_color: "black",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "circle",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: "",
+    border_hover_color: "#ff0000",
+    border_hover_size: "20"
+  },
+  state_specific: {
+    POL3139: {
+      name: "Warmińsko-Mazurskie"
+    },
+    POL3140: {
+      name: "Pomorskie"
+    },
+    POL3141: {
+      name: "Dolnośląskie"
+    },
+    POL3142: {
+      name: "Zachodniopomorskie"
+    },
+    POL3143: {
+      name: "Lubuskie"
+    },
+    POL3144: {
+      name: "Wielkopolskie"
+    },
+    POL3145: {
+      name: "Kujawsko-Pomorskie"
+    },
+    POL3146: {
+      name: "Śląskie"
+    },
+    POL3147: {
+      name: "Łódzkie"
+    },
+    POL3148: {
+      name: "Mazowieckie"
+    },
+    POL3149: {
+      name: "Świętokrzyskie"
+    },
+    POL3150: {
+      name: "Podlaskie"
+    },
+    POL3151: {
+      name: "Lubelskie"
+    },
+    POL3152: {
+      name: "Podkarpackie"
+    },
+    POL3167: {
+      name: "Opolskie"
+    },
+    POL3170: {
+      name: "Małopolskie"
+    }
+  },
+  locations: {},
+  labels: {},
+  regions: {}
+};
